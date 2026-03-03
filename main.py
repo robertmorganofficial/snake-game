d5849f2fa6630128d7e6978bcd797aa7349c8543
Robert Morgan <256670046+robertmorganofficial@users.noreply.github.com>
2026-03-03 13:50:34 -0500

    Implement game window setup and initial score display

--- main.py
+++ main.py
@@ -31,3 +31,37 @@ def check_collisions():
 
 def game_over():
     pass
+
+#Set game window settings
+window = Tk()
+window.title("Snake Game")
+window.resizable(False, False)
+
+#Define Score and initial direction
+score = 0
+direction = 'down'
+
+label = Label(window, text="Score:{}".format(score), font=('consolas', 40))
+label.pack()
+
+#Build Game Window
+canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
+canvas.pack()
+
+#Center Window when game appears
+window.update()
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width/2)) - int((window_width/2))
+y = int((screen_height/2)) - int((window_height/2))
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+#Create Snake Object and Food Object
+snake = Snake()
+food = Food()
+
+window.mainloop()
