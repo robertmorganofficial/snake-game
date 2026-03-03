a9a84691bdf5f6acd9828e34a90b6d1db95430cc
Robert Morgan <256670046+robertmorganofficial@users.noreply.github.com>
2026-03-03 14:51:33 -0500

    Implement Snake class with initialization and coordinate setup

--- main.py
+++ main.py
@@ -13,13 +13,25 @@ BACKGROUND_COLOR ="#000000"
 
 #Define Classes
 class Snake:
-    pass
+    def __init__(self):
+        self.body_size = BODY_PARTS
+        self.coordinates = []
+        self.squares = []
 
+        for i in range(0, BODY_PARTS):
+            self.coordinates.append([0, 0])
+
+        for x, y in self.coordinates:
+            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
+            self.squares.append(square)
 class Food:
     def __init__(self):
-        x = random.randint(0, (GAME_WIDTH/SPACE_SIZE) - 1) * SPACE_SIZE
-        y = random.randint(0, (GAME_HEIGHT/SPACE_SIZE) - 1) * SPACE_SIZE
+        x = random.randint(0, (GAME_WIDTH//SPACE_SIZE) - 1) * SPACE_SIZE
+        y = random.randint(0, (GAME_HEIGHT//SPACE_SIZE) - 1) * SPACE_SIZE
 
+        self.coordinates = [x, y]
+
+        canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
 
 #Define functions
 def next_turn():
