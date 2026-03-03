c10bddc7702c3dc8cb21acdbda09b6e2744524fa
Robert Morgan <256670046+robertmorganofficial@users.noreply.github.com>
2026-03-03 13:37:01 -0500

    Define game constants

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 from tkinter import *
 import random
 
+#Define Contants (Game settings we dont want to change):
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "#00FF00"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR ="#000000"
+
+#Define Classes
 class Snake:
     pass
 
 class Food:
     pass
 
+
+#Define functions
 def next_turn():
     pass
 
-def change_direction():
+def change_direction(new_direction):
     pass
 
 def check_collisions():
