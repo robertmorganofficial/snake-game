2f47eae3185865a137d673ff78602922c4a719a5
Robert Morgan <256670046+robertmorganofficial@users.noreply.github.com>
2026-03-03 10:17:05 -0500

    Initialize game window and set up basic game parameters

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Initial Code")
+import tkinter
+import random
+
+ROWS = 25
+COLS = 25
+TITLE_SIZE = 25
+
+WINDOW_WIDTH = TITLE_SIZE * COLS
+WINDOW_HEIGHT = TITLE_SIZE * ROWS
+
+#Intiralize Game Window
+window = tkinter.Tk()
+window.title("Snake")
+window.resizable(False,False)
+
+window.mainloop()
