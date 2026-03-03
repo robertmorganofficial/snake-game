b86ffc7306b2f5e8f251a8535da94b4345bc021a
Robert Morgan <256670046+robertmorganofficial@users.noreply.github.com>
2026-03-03 13:33:29 -0500

    Add initial game functions and classes for Snake and Food

--- main.py
+++ main.py
@@ -3,3 +3,18 @@ import random
 
 class Snake:
     pass
+
+class Food:
+    pass
+
+def next_turn():
+    pass
+
+def change_direction():
+    pass
+
+def check_collisions():
+    pass
+
+def game_over():
+    pass
